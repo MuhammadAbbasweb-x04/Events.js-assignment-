45e5860e6fcc6c6617d4ffd4b2b0e1e3304fe342
khalilsediq <khalilullahsadique1334@gmail.com>
2025-09-29 17:36:37 +0500

    added

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 // Q1
 // Answer
-let btn = document.querySelector("#btn");
-let body = document.querySelector("body")
-btn.addEventListener("click" , ()=>{
-body.style.backgroundColor = "blue";
 
-})
+// let colors = ["blue","red","green","yellow","black","gray"];
+// let btn = document.querySelector("#btn");
+// let body = document.querySelector("body")
+// btn.addEventListener("click" ,()=>{
+// colors.filter(color => color.includes() === colors.length )
+// })
+
+
+// Q2
+// Answer
+// let darkbtn = document.querySelector("#darkbtn ");
+// let lightbtn = document.querySelector("#lightbtn");
+// let body = document.querySelector("body");
+// let h1 = document.querySelector("h1");
+
+
+// darkbtn.addEventListener("click" , () =>{
+// body.style.backgroundColor = "black"
+// h1.style.color = "white";
+
+// })
+
+// lightbtn.addEventListener("click" , () =>{
+// body.style.backgroundColor = "white"
+// h1.style.color = "black";
+// })
+
+
+
+// Q3
+// Answer
+// let hide = document.querySelector("#btn-hide");
+// let h1 = document.querySelector("h1")
+// hide.addEventListener("click" , ()=>{
+// h1.style.display = "none";
+// })
+
+
+
+// Q4
+// Answer

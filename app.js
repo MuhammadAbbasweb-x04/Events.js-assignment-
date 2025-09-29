474bff7c99b844361234419bc271587fa6a3141f
khalilsediq <khalilullahsadique1334@gmail.com>
2025-09-29 15:57:35 +0500

    added

---
+++ app.js
@@ -0,0 +1,8 @@
+// Q1
+// Answer
+let btn = document.querySelector("#btn");
+let body = document.querySelector("body")
+btn.addEventListener("click" , ()=>{
+body.style.backgroundColor = "blue";
+
+})
